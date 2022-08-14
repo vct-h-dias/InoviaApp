a91ef556187cdf3e71ae41c00225371520b2d8c3
vct-h-dias <vhmontanaridias@gmail.com>
2022-08-13 23:14:18 -0300

    adição do preview da Foto 2 e criação da aba de finalizar (porém sem a captura de tela ainda)

---
+++ app/src/main/java/com/example/byyou/Finalizar.kt
@@ -0,0 +1,11 @@
+package com.example.byyou
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Finalizar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_finalizar)
+    }
+}
