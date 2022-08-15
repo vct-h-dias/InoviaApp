8e1f1afa39e115a2a394d67691e3318c745a3dfb
vct-h-dias <vhmontanaridias@gmail.com>
2022-08-14 21:36:21 -0300

    adição de listener aos cards do fragment Pessoal para também irem ao Paciente (igual o card da Agenda)

--- app/src/main/java/com/example/byyou/PessoalFragment.kt
+++ app/src/main/java/com/example/byyou/PessoalFragment.kt
@@ -1,24 +1,38 @@
 package com.example.byyou
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.byyou.databinding.FragmentPessoalBinding
 
 
-class PessoalFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+class PessoalFragment : Fragment(R.layout.fragment_pessoal) {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pessoal, container, false)
+        val binding = FragmentPessoalBinding.inflate(layoutInflater)
+
+
+        binding.cardView2.setOnClickListener(){
+            abrirPaciente()
+        }
+        binding.cardView3.setOnClickListener(){
+            abrirPaciente()
+        }
+        binding.cardView4.setOnClickListener(){
+            abrirPaciente()
+        }
+
+
+        return binding.root
+    }
+    private fun abrirPaciente(){
+        val i = Intent(this@PessoalFragment.requireContext(),Paciente::class.java)
+        startActivity(i)
     }
 }
