cc33bc6914644a5fd14dd669cee57f9ab8335f11
vct-h-dias <vhmontanaridias@gmail.com>
2022-08-15 23:58:16 -0300

    adição da seleção de outra área

--- app/src/main/java/com/example/byyou/Local.kt
+++ app/src/main/java/com/example/byyou/Local.kt
@@ -10,28 +10,58 @@ import androidx.appcompat.app.AppCompatActivity
 
 class Local : AppCompatActivity() {
 
-
-
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_local)
 
-        val bt = findViewById<Button>(R.id.toMed)
-        val quad = findViewById<ImageView>(R.id.square)
+
         val hitbox = findViewById<ImageView>(R.id.tr)
-        val textLocal = findViewById<TextView>(R.id.textL)
+        val hitbox2 = findViewById<ImageView>(R.id.tr2)
+
+
         hitbox.setOnClickListener{
-            bt.visibility = View.VISIBLE
-            quad.visibility = View.VISIBLE
-            textLocal.visibility = View.VISIBLE
+
+            VisibilidadeA()
+
         }
+        hitbox2.setOnClickListener{
+
+            VisibilidadeC()
+
+        }
+
+
+        val bt = findViewById<Button>(R.id.toMed)
+
 
         bt.setOnClickListener{
             irAreas()
         }
 
+    }
+
+
+    private fun VisibilidadeA(){
+        val bt1 = findViewById<Button>(R.id.toMed)
+        bt1.visibility = View.VISIBLE
+
+        val quad = findViewById<ImageView>(R.id.square)
+        quad.visibility = View.VISIBLE
+
+        val textLocal = findViewById<TextView>(R.id.textL)
+        textLocal.visibility = View.VISIBLE
+    }
+
+
+    private fun VisibilidadeC(){
+        val bt2 = findViewById<Button>(R.id.toMed)
+        bt2.visibility = View.VISIBLE
+
+        val quad2 = findViewById<ImageView>(R.id.squareC)
+        quad2.visibility = View.VISIBLE
+
+        val textLocalC = findViewById<TextView>(R.id.textLC)
+        textLocalC.visibility = View.VISIBLE
     }
 
 
