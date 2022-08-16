407ac94ef4fad58cca4f8da4b894b0d280522200
vct-h-dias <vhmontanaridias@gmail.com>
2022-08-15 22:49:00 -0300

    adição da aba 1 medição por foto (reutilização da aba de medição e da aba de foto)

--- app/src/main/java/com/example/byyou/Med1_1.kt
+++ app/src/main/java/com/example/byyou/Med1_1.kt
@@ -5,26 +5,17 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
-class Med1 : AppCompatActivity() {
+class Med1_1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_med1)
+        setContentView(R.layout.activity_med11)
 
-        val btFoto1: Button = findViewById(R.id.toFoto1)
-        btFoto1.setOnClickListener{
+        val btFoto1_2: Button = findViewById(R.id.toFoto1_2)
+        btFoto1_2.setOnClickListener{
             abrirFoto1()
         }
 
-        val btMedFt1: Button = findViewById(R.id.toMedFt1)
-        btMedFt1.setOnClickListener{
-            abrirMeedFt1()
-        }
 
-    }
-
-    private fun abrirMeedFt1(){
-        val inte = Intent(this,MedFt1::class.java)
-        startActivity(inte)
     }
 
     private fun abrirFoto1(){
